43e0c58a76b49df87ee973bf5a326822e86bfe41
Saoud <saoud@saoud.dev>
2021-03-08 22:17:14 -0800

    adding new challenge, Create a function that takes voltage and current and returns the calculated power.

---
+++ Power Calculator.js
@@ -0,0 +1,8 @@
+//Create a function that takes voltage and current and returns the calculated power.
+//https://edabit.com/challenge/wAdE9te55cowBLcPs
+
+
+function circuitPower(voltage, current) {
+	power = voltage * current
+	return power
+}
