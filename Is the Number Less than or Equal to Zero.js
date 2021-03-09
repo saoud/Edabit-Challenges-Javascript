69c5a1a2fc2f8769c2b638a04427e7a1fc9e6b00
Saoud <saoud@saoud.dev>
2021-03-08 23:39:02 -0800

    adding 3 more questions and answers, promises III is not complete yet

---
+++ Is the Number Less than or Equal to Zero.js
@@ -0,0 +1,16 @@
+// Is the Number Less than or Equal to Zero?
+// https://edabit.com/challenge/PTiLYyb4A69KZtBCg
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+// Examples
+// lessThanOrEqualToZero(5) ➞ false
+// lessThanOrEqualToZero(0) ➞ true
+// lessThanOrEqualToZero(-2) ➞ true
+
+
+function lessThanOrEqualToZero(num) {
+	if (num <= 0) {
+  return true;
+} else if (num >= 0);
+return false;
+}
---
+++ Promises III: Native Promise, Introducing the Executor.js
@@ -0,0 +1,12 @@
+// Promises are just objects that contain the outcome of asynchronous operations. So when do you use one? When you want to control the outcome of an asynchronous operation. All you have to do is wrap the asynchronous function with a promise constructor.
+// https://edabit.com/challenge/8kTQqoWYQXRsKuYEf
+// The promise constructor requires you to pass a function called the executor which takes two parameters, resolve and reject. Both are functions that you use to pass or reject a value that is usually the result of the async operation. Here's an example of a simple promise:
+
+// let promise = new Promise( (resolve, reject) => {
+//   setTimeout(( ) => {
+//      resolve("edabit")
+//   }, 1000)
+// })
+// setTimeout is a browser API that is very commonly used in tutorials to represent async operations. After 1000ms has passed, we call the callback function in setTimeout() and pass a string "edabit" to the resolve function.
+
+// Create a simple promise and pass the resolve function a string value of your choice. Use the setTimeout function as your asynchronous operation. Your setTimeout() function should not exceed 1000ms. Store the promise inside a variable named promise.
